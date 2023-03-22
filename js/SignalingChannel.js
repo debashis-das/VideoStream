0101d3cdb87db2301a0dd993f9ff25f9186542d9
Debashis Das <debashisdas@Debashiss-MacBook-Pro.local>
2023-03-22 12:09:56 +0530

    Debashis | Running code with candidate error

--- js/SignalingChannel.js
+++ js/SignalingChannel.js
@@ -1,33 +1,81 @@
-var ice = {
-    //     "iceServers": [
-    //     {"url": "stun:stunserver.com:12345"}
-    //   ]
-    };
-var pc = new RTCPeerConnection(ice);
+const ice = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+const remoteVideo = document.querySelector('video#remote_video');
+const videoElement = document.querySelector('video#local_video');
 
 class SignalingChannel {
+
     constructor(){
         this.ws = new WebSocket("ws://localhost:8765");
+        this.remote = []
+        this.local = null
         this.init()
+    }
+
+    addLocal(localPeer){
+        this.local = localPeer
+    }
+
+    getConnection(){
+        return this.ws
+    }
+
+    peerConnected(){
+        this.remotePeer[0].addEventListener('track', async (event) => {
+            const [remoteStream] = event.streams;
+            remoteVideo.srcObject = remoteStream;
+        });
     }
 
     init(){
         this.ws.onopen = function() {
-            // Web Socket is connected, send data using send()
-            this.pId = crypto.randomUUID()
-            this.pType = 'WebBrowser'
-            this.send(JSON.stringify({eventType : 'READY', peerId : this.pId , peerType : this.pType}));
-            console.log("Message is sent...");
-            // ERROR
-            // ws.send(JSON.stringify({eventType : 'READY', peerId : uuid , peerType : 'WebBrowser'}));
-            // console.log("Message is sent... again");
-            // ws.send(JSON.stringify({eventType : 'READY', peerId : crypto.randomUUID() , peerType : 'WebBrowser'}));
+            this.peerId = Math.floor(Math.random()*90000) + 10000;
+            this.send(JSON.stringify({eventType : 'READY', peerId : this.peerId }));
+            console.log("websocket init...");
         };
         
         this.ws.onclose = function() { 
-            // websocket is closed.
             console.log("Connection is closed..."); 
         };
+
+        this.ws.addEventListener('message', async message => { 
+            try{
+                if(JSON.parse(message.data).eventType == 'ANSWER') {   
+                    const remoteDesc = new RTCSessionDescription(JSON.parse(message.data).answer);
+                    await this.local.setRemoteDescription(remoteDesc);
+                }else if(JSON.parse(message.data).eventType == 'OFFER'){
+                    this.remote[0].setRemoteDescription(new RTCSessionDescription(JSON.parse(message.data).offer));
+                    const answer = await this.remote[0].createAnswer();
+                    await this.remote[0].setLocalDescription(answer);
+                    this.ws.send(JSON.stringify({eventType : 'ANSWER', answer : answer, peerId : this.ws.peerId}));
+                }else if(JSON.parse(message.data).eventType  == 'CANDIDATE'){
+                    if (JSON.parse(message.data).iceCandidate) {
+                        try {
+                            await this.local.addIceCandidate(JSON.parse(message.data).iceCandidate);
+                        } catch (e) {
+                            console.error('Error adding received ice candidate', e);
+                        }
+                    }
+                }else if(JSON.parse(message.data).eventType  == 'PEER'){
+                    const remotePeer = new RTCPeerConnection(ice);
+                    this.remote.push(remotePeer)
+                    console.log("peer added...")
+                    //offer stream
+                    const offer = await this.local.createOffer();
+                    await this.local.setLocalDescription(offer);
+                    this.ws.send(JSON.stringify({eventType : 'OFFER', offer : offer, peerId : this.ws.peerId}));
+                    this.local.addEventListener('icecandidate', event => {
+                        if (event.candidate) {
+                            this.ws.send(JSON.stringify({eventType : 'CANDIDATE', peerId : this.ws.peerId, iceCandidate : event.candidate}));
+                        }
+                    });
+                }else{
+                    console.log("ERROR...")
+                    console.log(JSON.parse(message.data))
+                }
+            }catch(error){
+                console.log(error)
+            }
+        });
     }
 }
 
@@ -36,32 +84,27 @@ var constraints = {
     video: true
 }
 
-
-
-
 // webrtc
 var signalingChannel = new SignalingChannel();
-const remoteVideoELement = document.querySelector('video#remote_video');
-const videoElement = document.querySelector('video#local_video');
+
 
 async function playVideoFromCamera() {
-    try {
+    try { 
+        var pc = new RTCPeerConnection(ice);
+        signalingChannel.addLocal(pc)
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         //local video
-        console.log(stream)
         videoElement.srcObject = stream;
-        //offer stream
-        pc.addStream(stream);
-        pc.createOffer().then((offer) => pc.setLocalDescription(offer))
-            .then(() => {
-                signalingChannel.ws.send(JSON.stringify({
-                    eventType: "OFFER",
-                    sdp: pc.localDescription,
-                  }))
-            })
-            .catch((reason) => {
-                console.error(reason)
-            });
+        console.log("waiting for peer...")
+        stream.getTracks().forEach(track => {
+            pc.addTrack(track, stream);
+        })
+        pc.addEventListener('connectionstatechange', event => {
+            if (peerConnection.connectionState === 'connected') {
+                console.log("PEER CONNECTED !!!!!")
+                signalingChannel.peerConnected()
+            }
+        });
     } catch(error) {
         console.error('Error opening video camera.', error);
     }
@@ -71,55 +114,8 @@ playVideoFromCamera();
 
 
 
-signalingChannel.ws.onmessage = function (evt) { 
-    try{
-        if(typeof JSON.parse(evt.data) == "string"){
-            var received_msg = JSON.parse(JSON.parse(evt.data));
-        }else{
-            var received_msg = JSON.parse(evt.data);
-        }
-        console.log("Message is received... --> ");
-        console.log(received_msg);
-        if (received_msg.eventType == 'CANDIDATE' && received_msg.message) {   
-            pc.addIceCandidate(received_msg.message);
-        }else if(received_msg.eventType == 'OFFER'){
-            console.log("onmessage !! offer -->")
-            remote_pc = new RTCPeerConnection(ice);
-            remote_pc.setRemoteDescription(received_msg.sdp);
-            remote_pc.onicecandidate = function(evt) {
-              if (evt.candidate) {
-                console.log("remote onicecandidate--->")
-                signalingChannel.ws.send(JSON.stringify({eventType : 'CANDIDATE', message: evt.candidate}));
-              }
-            }
-            remote_pc.addEventListener('track', async (event) => {
-                console.log("remote_pc addEventListener")
-                const [remoteStream] = event.streams;
-                remoteVideoELement.srcObject = remoteStream;
-            });
-        }else{
-            console.log("onmessage !! else -->")
-        }
-    }catch(error){
-        console.error(error)
-    }
-}
 
-pc.onicecandidate = function(evt) { 
-    if (evt.candidate) {
-        signalingChannel.ws.send(JSON.stringify({eventType : 'CANDIDATE', message: evt.candidate}));
-    }
-}
 
-pc.oniceconnectionstatechange = function(evt) { 
-    console.log("ICE connection state change: " + evt.target.iceConnectionState);
-}
-
-pc.addEventListener('track', async (event) => {
-    console.log("pc addEventListener")
-    const [remoteStream] = event.streams;
-    remoteVideoELement.srcObject = remoteStream;
-});
 
 
 
